6e1fd55acf947d2e8d88fe8359f4abe77fcb1e32
0xjl <jdl.lourens@gmail.com>
2025-03-23 19:25:28 +0800

    refresher scrimba project 2

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="nav-header">
+            <img src="../images/globe.png" alt="globe icon" />
+            <h1>my travel journal</h1>
+            <img src="../images/globe.png" alt="globe icon" />
+        </header>
+    )
+}
